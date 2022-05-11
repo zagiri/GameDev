4f284a681c0eb8d72c309f4967c2f1b7cf33ff06
trent <Trentrmb2003@gmail.com>
2022-05-11 19:24:05 +1200

    Added comments to my Javascript

--- assets/ContactformValidation.js
+++ assets/ContactformValidation.js
@@ -1,77 +1,3 @@
-// function inputCheck() {
-//     var firstname = document.getElementById("firstname").value;
-//     var lastname = document.getElementById("lastname").value;
-//     var address = document.getElementById("address").value;
-//
-//     document.getElementById("fnameerror").innerHTML = "";
-//     document.getElementById("lnameerror").innerHTML = "";
-//     document.getElementById("adderror").innerHTML = "";
-//
-//     var email = document.getElementById("email").value;
-//     var mailformat = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
-//     if (!email.match(mailformat)) {
-//         alert("Must enter a valid email address");
-//         document.customerInfoForm.email.focus();
-//         return false;
-//     }
-//     if (firstname === "" || firstname === null) {
-//         document.getElementById("fnameerror").innerHTML = "Please enter a first name";
-//         return false;
-//     }
-//     if (lastname === "" || lastname === null) {
-//         document.getElementById("lnameerror").innerHTML = "Please enter a last name";
-//         return false;
-//     }
-//     if (address === "" || address === null) {
-//         document.getElementById("adderror").innerHTML = "Please enter an email address";
-//         return false;
-//     }
-//     alert("Thanks for submitting your form");
-//     resetInfo();
-// }
-//
-// function resetInfo() {
-//     document.getElementById("titleDropList").value = "ms";
-//     var i;
-//     var textboxes = document.getElementsByClassName("guestInfoTextBox");
-//     for (i = 0; i < textboxes.length; i++) {
-//         textboxes[i].value = "";
-//     }
-//     document.getElementById("adderror").style.display = "none";
-//     document.getElementById("fnameerror").style.display = "none";
-//     document.getElementById("lnameerror").style.display = "none";
-// }
-// form.addEventListener('submit', (e) => {
-//     console.log("anything")
-//     e.preventDefault()
-// });
-// function inputCheck() {
-//     let x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//         alert("This is required to be filled out")
-//         return false;
-//     }
-//     let x = document.forms["myForm"]["lname"].value;
-//     if (x == "") {
-//         alert("This is required to be filled out")
-//         return false;
-//     }
-//     let x = document.forms["myForm"]["emailaddress"].value;
-//     if (x == "") {
-//         alert("This is required to be filled out")
-//         return false;
-//     }
-//     if ( document.getElementById('country').selectedIndex == 0 ) {
-//         alert ( "This is required to be filled out" );
-//         return false;
-//     }
-//     let x = document.forms["myForm"]["subject"].value;
-//     if (x == "") {
-//         alert("This is required to be filled out")
-//         return false;
-//     }
-//
-// }
 function validate(){
     var name = document.getElementById("name").value;
     var phone = document.getElementById("phone").value;
@@ -81,23 +7,26 @@ function validate(){
     var error_message = document.getElementById("error_message");
 
     error_message.style.padding = "10px";
-
+    //Making sure the name is long enough to be an actual name
     var text;
-    if(name.length < 4){
+    if(name.length == "" || name.length == null){
         text = "Please Enter valid Name";
         error_message.innerHTML = text;
         return false;
     }
+    //Making sure they have input a valid phone number
     if(isNaN(phone) || phone.length != 10){
         text = "Please Enter valid Phone Number";
         error_message.innerHTML = text;
         return false;
     }
+    //Making sure their email is valid
     if (!email.match(mailformat) || email.length < 6){
         text = "Please Enter valid Email";
         error_message.innerHTML = text;
         return false;
     }
+    //Making sure the contacter has put a message in the contact form
     if(message.length <= 0){
         text = "Please Enter a valid message";
         error_message.innerHTML = text;
