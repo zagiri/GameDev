26089b5291f27feb861d4bce30c76ed368080ddc
trent <Trentrmb2003@gmail.com>
2022-05-12 15:23:39 +1200

    Added comments and made the titles of each page relevant

--- assets/JavaScript/ContactformValidation.js
+++ assets/JavaScript/ContactformValidation.js
@@ -7,28 +7,32 @@ function validate(){
     var error_message = document.getElementById("error_message");
 
     error_message.style.padding = "10px";
-
+    //Validation of Name
     var text;
     if(name.length < 4){
         text = "Please Enter valid Name";
         error_message.innerHTML = text;
         return false;
     }
+    //Validation of Phone Number
     if(isNaN(phone) || phone.length != 10){
         text = "Please Enter valid Phone Number";
         error_message.innerHTML = text;
         return false;
     }
+    //Validation of Email
     if (!email.match(mailformat) || email.length < 6){
         text = "Please Enter valid Email";
         error_message.innerHTML = text;
         return false;
     }
+    //Validation of Message
     if(message.length <= 0){
         text = "Please Enter a valid message";
         error_message.innerHTML = text;
         return false;
     }
+    //Alerting user of form being submitted
     alert("Form Submitted Successfully!");
     return true;
 }
